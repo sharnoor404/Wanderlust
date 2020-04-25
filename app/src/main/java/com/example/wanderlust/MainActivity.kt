f0613ff81978b9ef74e6153d2b555f17df7cb4a6
sharnoor404 <sharnoor404@gmail.com>
2020-04-25 22:53:55 +0530

    step 2

--- app/src/main/java/com/example/wanderlust/MainActivity.kt
+++ app/src/main/java/com/example/wanderlust/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.wanderlust
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -20,6 +21,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
+        //step 2-when Add Place option of menu is clicked the user is navigated to a new intent(Maps Activity)
+        //!! sign represents that item value surely has some value
+        
+        if(item!!.itemId==R.id.add_place){
+            val intent= Intent(applicationContext,MapsActivity::class.java)
+            startActivity(intent)
+
+        }
 
         return super.onOptionsItemSelected(item)
     }
